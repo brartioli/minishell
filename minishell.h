2565203fccb94f150a5ad9cd80aa6bd7fbf42232
brunaartioli.ba@gmail.com <bfernan2@c1r3p1.42sp.org.br>
2026-01-13 19:14:14 -0300

    feat: header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,17 @@
 /*   By: bfernan2 <bfernan2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:18:52 by bfernan2          #+#    #+#             */
-/*   Updated: 2026/01/13 18:18:53 by bfernan2         ###   ########.fr       */
+/*   Updated: 2026/01/13 19:13:03 by bfernan2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <fcntl.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+#endif
