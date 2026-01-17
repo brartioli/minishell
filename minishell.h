4aa47e5790c33d0ac7dae6624197d59a5c72b379
Malu Araújo <araujomaluc@gmail.com>
2026-01-17 13:41:39 -0300

    feat: structs token and env

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malcosta <malcosta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maluojuara <maluojuara@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:18:52 by bfernan2          #+#    #+#             */
-/*   Updated: 2026/01/13 19:21:15 by malcosta         ###   ########.fr       */
+/*   Updated: 2026/01/17 13:40:10 by maluojuara       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,31 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft/libft.h"
+
+// Define token types
+# define TYPE_CMD "CMD"
+# define TYPE_ARG "ARG"
+# define TYPE_PIPE "PIPE"
+
+
+// Structs
+typedef struct s_token
+{
+	char	*value;
+	char	*type;
+	struct s_token	*next;
+	struct s_token	*prev;
+}		t_token;
+
+
+typedef struct s_env
+{
+	char	*name;
+	char	*value;
+	struct s_env	*next;
+}	t_env;
+
+// Functions
+
 
 #endif
